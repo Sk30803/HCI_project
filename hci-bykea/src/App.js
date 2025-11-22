3bb79b1e895616be660e312bf4d6453d0b7d3f99
Sk30803 <sahil.k30803@gmail.com>
2025-11-22 16:59:16 +0500

    reset the homepage to old design

--- hci-bykea/src/App.js
+++ hci-bykea/src/App.js
@@ -39,6 +39,13 @@ function App() {
         </button>
       </header>
 
+      {/* Safety bar */}
+      <div className="safety-strip">
+        <button className="safety-button primary">SOS</button>
+        <button className="safety-button">Share Trip</button>
+        <button className="safety-button">Help</button>
+      </div>
+
       {/* Main Content */}
       <main className="app-main">{renderScreen()}</main>
 
